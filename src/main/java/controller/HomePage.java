e85d132409ee3dd54a554e06ea88e4091e69ab6e
DESKTOP-KMNS09Q\Admin <nguyenvantai6899@gmail.com>
2023-05-23 16:56:32 +0700

    no message

--- src/main/java/controller/HomePage.java
+++ src/main/java/controller/HomePage.java
@@ -6,7 +6,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomePage {
     @GetMapping({"/","homePage"})
-    public String HomePage(){
+    public String Home_Page(){
         return "homePage";
+    }
+    @GetMapping("room")
+    public String Room(){
+        return "room";
+    }
+    @GetMapping("about")
+    public String About(){
+        return "about";
+    }
+    @GetMapping("booking")
+    public String Booking(){
+        return "booking";
+    }
+    @GetMapping("contact")
+    public String Contact(){
+        return "contact";
+    }
+    @GetMapping("service")
+    public String Service(){
+        return "service";
     }
 }
